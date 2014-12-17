64fc747f9fd0ac9096c8d754a1412dbf82c286b9
ablease <alexblease@gmail.com>
2014-12-17 12:03:26 +0000

    Adding Validations and reviews

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe Review, :type => :model do
+  scenario 'is invalid if the rating is more than 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end  
+end
