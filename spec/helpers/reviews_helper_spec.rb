13707bee0bae3eb8cd54c198e41e8a4b4129d633
ablease <alexblease@gmail.com>
2014-12-18 16:42:32 +0000

    unit test for star_rating helper method

--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -16,5 +16,9 @@ describe ReviewsHelper, :type => :helper do
       expect(helper.star_rating(3)).to eq '★★★☆☆'
     end
 
+    it 'returns four black stars and one white star for 3.5' do
+      expect(helper.star_rating(3.5)).to eq '★★★★☆'
+    end
+
   end
 end
