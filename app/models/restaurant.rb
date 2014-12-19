2dbebf99eea7ab10559c0b087f8fb82d780e2138
ed <ejbyne@gmail.com>
2014-12-19 16:53:45 +0000

    aws working locally

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,13 +7,10 @@ class Restaurant < ActiveRecord::Base
   validates :name, length: {minimum: 3}, uniqueness: true
 
   has_attached_file :image,
-                    :styles => { :medium => "300x300", :thumb => "100x100" },
-                    :default_url => "/images/:style/missing.png",
-                    :s3_host_name => 's3-us-west-2.amazonaws.com'
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => ":style/missing.png"
 
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
-
-  attr_accessor :image_file_name
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def average_rating
     return 'N/A' if reviews.none?
