3e25f1f752ce629b4b251ff54d5893852208c609
ed <ejbyne@gmail.com>
2014-12-19 15:30:39 +0000

    added attribute accessor to restaurant class

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,6 +9,7 @@ class Restaurant < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
 
+  attr_accessor :image_file_name
 
   def average_rating
     return 'N/A' if reviews.none?
