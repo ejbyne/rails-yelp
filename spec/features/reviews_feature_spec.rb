e8da4aab182e0fd0acef9e56da3211767cb8bfef
ed <ejbyne@gmail.com>
2014-12-23 14:42:57 +0100

    refactored review tests

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'reviewing'  do
+feature 'reviews'  do
 
   def leave_review(thoughts, rating)
     visit '/restaurants'
@@ -30,41 +30,55 @@ feature 'reviewing'  do
     leave_review('so so', '3')
   end
 
-  scenario 'allows users to leave a review using a form' do
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content('so so')
-  end
+  context 'leaving reviews' do
 
-  scenario 'user cannot leave more than 1 review per restaurant' do
-    visit '/restaurants'
-    click_link 'Review KFC'
-    expect(page).to have_content('You cannot leave more than 1 review per restaurant')
-  end
+    scenario 'allows users to leave a review using a form' do
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content('so so')
+    end
 
-  scenario 'user can delete a review he has created' do
-    visit '/restaurants'
-    click_link "Delete this review"
-    expect(page).to have_content("Review deleted successfully")
-  end
-
-  scenario "user cannot delete a review he hasn't created" do
-    click_link('Sign out')
-    another_user_sign_up
-    visit '/restaurants'
-    expect(page).not_to have_content 'Delete this review'
+    scenario 'user cannot leave more than 1 review per restaurant' do
+      visit '/restaurants'
+      click_link 'Review KFC'
+      expect(page).to have_content('You cannot leave more than 1 review per restaurant')
+    end
 
   end
 
-  scenario 'average ratings' do
-    click_link('Sign out')
-    another_user_sign_up
-    leave_review('Great', '5')
-    expect(page).to have_content('Average rating: ★★★★☆')
+  context 'deleting reviews' do
+
+    scenario 'user can delete a review he has created' do
+      visit '/restaurants'
+      click_link "Delete this review"
+      expect(page).to have_content("Review deleted successfully")
+    end
+
+    scenario "user cannot delete a review he hasn't created" do
+      click_link('Sign out')
+      another_user_sign_up
+      visit '/restaurants'
+      expect(page).not_to have_content 'Delete this review'
+    end
+
   end
 
-  # scenario 'time created' do
-  #   leave_review('Great', '5')
-  #   expect(page).to have_content('Review created: 1 hour ago')
-  # end
+  context 'average ratings' do
+
+    scenario 'displays average ratings' do
+      click_link('Sign out')
+      another_user_sign_up
+      leave_review('Great', '5')
+      expect(page).to have_content('Average rating: ★★★★☆')
+    end
+
+  end
+
+  context 'time created' do
+
+    scenario 'time created' do
+      expect(page).to have_content('Review created: 1 hour ago')
+    end
+
+  end
 
 end
