719a41545716407a7726aa057f530840cb896e45
ed <ejbyne@gmail.com>
2014-12-23 17:30:53 +0100

    added pluralize method for displaying time created

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -6,7 +6,7 @@ module ReviewsHelper
   end
 
   def time_since_created(time_created)
-    return ((Time.now - time_created) / 3600).round.to_s + " hours ago" if (Time.now - time_created) < 84600
-    ((Time.now - time_created) / 86400).round.to_s + " days ago"
+    return pluralize((((Time.now - time_created) / 3600).round.to_s), 'hour') + " ago" if (Time.now - time_created) < 84600
+    pluralize((((Time.now - time_created) / 86400).round.to_s), 'day') + " ago"
   end
 end
